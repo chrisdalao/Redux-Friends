e56b2e8c0c1f7a7c2f7c4a21ca2fb534dccfa80d
Chris Dalao <chrisdalao@gmail.com>
2019-06-20 19:05:42 -0700

    Login form added on Login Component - action and reducer WIP

--- redux-friends/src/components/Login.js
+++ redux-friends/src/components/Login.js
@@ -1,8 +1,40 @@
 import React, { Component } from "react";
+import Loader from "react-loader-spinner";
 
 export class Login extends Component {
+  state = {
+    credentials: {
+      username: "",
+      password: ""
+    }
+  };
+
   render() {
-    return <div>Login Form</div>;
+    return (
+      <div>
+        <form onSubmit={this.login}>
+          <input
+            type="text"
+            name="username"
+            value={this.state.credentials.username}
+            onChange={this.handleChange}
+          />
+          <input
+            type="password"
+            name="password"
+            value={this.state.credentials.password}
+            onChange={this.handleChange}
+          />
+          <button>
+            {this.props.loggingIn ? (
+              <Loader type="ThreeDots" color="#1f2a38" height="12" width="26" />
+            ) : (
+              "Log in"
+            )}
+          </button>
+        </form>
+      </div>
+    );
   }
 }
 
