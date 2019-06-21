eb756e8a582d76852216ec87af321b4cd60bf748
Chris Dalao <chrisdalao@gmail.com>
2019-06-20 23:23:30 -0700

    friends are now rendering

--- redux-friends/src/components/Friends.js
+++ redux-friends/src/components/Friends.js
@@ -1,6 +1,7 @@
 import React, { Component } from "react";
 import { connect } from "react-redux";
 import { getFriends } from "../actions";
+import Friend from "./Friend";
 
 export class Friends extends Component {
   componentDidMount() {
@@ -8,7 +9,15 @@ export class Friends extends Component {
   }
 
   render() {
-    return <div>FRIEND LIST</div>;
+    console.log(this.props);
+    return (
+      <div className="friends-list">
+        FRIENDS LIST
+        {this.props.friends.map(friend => {
+          return <Friend key={friend.id} friend={friend} />;
+        })}
+      </div>
+    );
   }
 }
 
